fac7fb9e0cb34e7d5aef99d8ba1787fdb953c6ca
Karolis-Kvatavicius <39552171+Karolis-Kvatavicius@users.noreply.github.com>
2018-09-20 17:55:42 +0300

    final

--- resources/views/mechanic/mechanic_trucks.blade.php
+++ resources/views/mechanic/mechanic_trucks.blade.php
@@ -2,6 +2,10 @@
 <a href="{{route('mechanic.index')}}">Grįžti</a>
 <h1>{{$mechanic[0]->name}} {{$mechanic[0]->surname}}</h1>
 <ul>
+    {{-- {{ dd($trucks) }} --}}
+   @empty($trucks[0])
+        <li>Mechanikas neturi darbo</li>
+   @endempty
     @foreach ($trucks as $key => $truck)
        <li>{{$key+1}}. {{$truck->maker}} {{$truck->make_year}} {{$truck->plate}}</li> 
     @endforeach
